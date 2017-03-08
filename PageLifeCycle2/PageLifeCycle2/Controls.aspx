63c313eacee72c3385e692e8b26ccf16d2ad5e9d
JAX <JAX@DEADEYE>
2017-03-08 13:54:27 -0500

    added 2 pages to the pageLifeCycle2 project.

---
+++ PageLifeCycle2/PageLifeCycle2/Controls.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Controls.aspx.cs" Inherits="PageLifeCycle2.Controls" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>New Page Fo Yo....  </title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="btnSubmit1" runat="server" Text="Submit" OnClick="btnSubmit1_Click" />
+
+        <asp:Label ID="lblName" runat="server" Text=""></asp:Label>
+
+
+
+
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ PageLifeCycle2/PageLifeCycle2/Controls.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PageLifeCycle2
+{
+    public partial class Controls : System.Web.UI.Page
+    {
+
+        protected void Page_InitComplete(object sender, EventArgs e)
+        {
+            lblName.Text = lblName.Text + "<br/>" + "Init Complete";
+        }
+
+
+        protected override void OnPreLoad(EventArgs e)
+        {
+            lblName.Text = lblName.Text + "<br/>" + "Load";
+        }
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            lblName.Text = lblName.Text + "<br/>" + "Load";
+
+
+        }
+
+        protected void btnSubmit1_Click(object sender, EventArgs e)
+        {
+            lblName.Text = lblName.Text + "<br/>" + "btnSubmit_Click";
+        }
+
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        {
+            lblName.Text = lblName.Text + "<br/>" + "LoadComplete";
+        }
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            lblName.Text = lblName.Text + "Pre Render";
+        }
+
+
+        protected override void OnSaveStateComplete(EventArgs e)
+        {
+            lblName.Text = lblName.Text + "Save State Complete";
+        }
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            lblName.Text = lblName.Text + "Unload";
+        }
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            lblName.Text = lblName.Text + "PreInit";
+        }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            lblName.Text = lblName.Text + "Init";
+        }
+
+    }
+}
