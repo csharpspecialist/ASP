1531bf70a9b2ae3946fc97998490e6311f3acad0
csharpspecialist <netspec1@hotmail.com>
2017-03-16 02:50:26 -0400

    Built a Bob 1st MVC app

---
+++ HelloPrac1/HelloPrac1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloPrac1.Models;
+
+namespace HelloPrac1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Words = "Whatever!!!!";
+            ViewBag.Myst = "Get to the chopper!!!";
+            //Car auto = new Car();
+
+            
+            var vehicles = HelloModel.GetCarInfo();
+           
+
+            return View(vehicles);
+        }
+    }
+}
