0ca149bb8bcc17c54b54b110729d2c29f934ffb0
csharpspecialist <netspec1@hotmail.com>
2017-03-22 04:28:35 -0400

    Started the Treehouse MVC project

---
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+    }
+}
