f68102aaf29f39a3d4b7705a7a9922bb70d587dc
csharpspecialist <netspec1@hotmail.com>
2017-03-25 22:45:29 -0400

    Added Comic book and Artists models

---
+++ ComicBookGallery/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int BookID { get; set; }
+        public string  SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHTML { get; set; }
+        public Artists [] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+
+    }
+}
