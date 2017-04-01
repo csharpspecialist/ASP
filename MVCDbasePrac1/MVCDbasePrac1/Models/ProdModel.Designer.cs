241c5b3fcd546d681743901991d0d63933c39e7a
JAX <JAX@DEADEYE>
2017-04-01 15:59:20 -0400

    changed the Dbase prodname varchar field length of the MVCDbaseprac1 project

--- MVCDbasePrac1/MVCDbasePrac1/Models/ProdModel.Designer.cs
+++ MVCDbasePrac1/MVCDbasePrac1/Models/ProdModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\wor\ASP\MVCDbasePrac1\MVCDbasePrac1\Models\ProdModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Coding\Gitfiles\webprojects\ASP\MVCDbasePrac1\MVCDbasePrac1\Models\ProdModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
