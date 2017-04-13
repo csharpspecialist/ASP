d745ce15fa1d7bf5c92c76a5cdb4a2d75ee40421
csharpspecialist <netspec1@hotmail.com>
2017-04-13 01:50:12 -0400

    Finished the fitness frog Pt 4 of the course.  Started working on the 5th and final section of the course.  added the Mvc Music Store file and just started working on its problems

---
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        // GET: Store
+        public ActionResult Index()
+        {
+            var genre = storeDB.Genre.ToList();
+
+            return View(genre);
+        }
+
+        // GET: /Store/Browse?genre=Disco
+        public ActionResult Browse(string genre)
+        {
+            // Retrieve Genre and its Associated Albums from Database
+            //var genreModel = new Genre { Name = genre };
+            var genreModel = storeDB.Genre.Include("Album")
+                .Single(g => g.Name == genre);
+
+            return View(genreModel);
+        }
+
+        // GET: /Store/Details/5
+        public ActionResult Details(int id)
+        {
+            //var album = new Album { Title = "Album " + id };
+            var album = storeDB.Album.Find(id);
+
+            return View(album);
+        }
+    }
+}
