26992034a3acabc19c758ccc0dc80cea45bc73bf
csharpspecialist <netspec1@hotmail.com>
2017-05-14 23:57:27 -0400

    added a button to the create page.  Created a new connection string for the home system database inside the web.config file.

---
+++ NerdSolo/NerdSolo/Models/NerdDinners.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace NerdSolo.Models
+{
+    public class NerdDinners : DbContext
+    {
+        public DbSet<Dinner> Dinners { get; set; }
+        public DbSet<RSVP> RSVPs{ get; set; }
+
+
+
+
+    }
+}
