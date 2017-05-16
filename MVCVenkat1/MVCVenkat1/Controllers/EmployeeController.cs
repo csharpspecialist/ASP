2d1d3dbd98656245368f0c1ed0391f332612fb5d
csharpspecialist <netspec1@hotmail.com>
2017-05-16 08:20:04 -0400

    updated Dinner for nerds and also working on MVC Venkat solution.  Working on part 8 now

---
+++ MVCVenkat1/MVCVenkat1/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCVenkat1.Models;
+
+namespace MVCVenkat1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Details()
+        {
+            Employee worker = new Employee()
+            {
+                EmployeeId = 2,
+                Name = "Jaques",
+                Gender = "Male",
+                City = "Aqua"
+
+            }; 
+
+       
+            //if you don't pass the worker object 
+            //below you will get a null reference
+            //exception...your code blows up!!!!
+            return View(worker);
+        }
+    }
+}
